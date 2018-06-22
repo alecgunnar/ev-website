4569c035bed763003d25572d500cfd4c65ba373e
alecgunnar <alecgunnar@gmail.com>
2018-06-22 17:10:29 -0400

    Adds api

---
+++ api/src/main/kotlin/com/whatsupev/WhatsUpEV/WhatsUpEvApplication.kt
@@ -0,0 +1,11 @@
+package com.whatsupev.WhatsUpEV
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhatsUpEvApplication
+
+fun main(args: Array<String>) {
+    runApplication<WhatsUpEvApplication>(*args)
+}
---
+++ api/src/test/kotlin/com/whatsupev/WhatsUpEV/WhatsUpEvApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.whatsupev.WhatsUpEV
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class WhatsUpEvApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
