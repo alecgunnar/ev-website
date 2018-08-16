1a52f9e24a98098298598dc165c6aad0a95c08b8
alecgunnar <alecgunnar@gmail.com>
2018-08-15 22:30:47 -0400

    Automates full app start-up

--- release.sh
+++ release.sh
@@ -2,16 +2,17 @@
 
 set -e
 
+./gradlew startApp
+npm --prefix ./ui run test:e2e
+
 pushd api
 ./gradlew build
 popd
 
 pushd ui
 npm run test:unit
-npm run test:e2e
 npm run build
 popd
 
 scp api/build/libs/WhatsUpEV-RELEASE.jar $WHATSUPEV_DEPLOY_USER@$WHATSUPEV_DEPLOY_SERVER:$WHATSUPEV_DEPLOY_API_LOCATION
 scp -r ui/dist/*  $WHATSUPEV_DEPLOY_USER@$WHATSUPEV_DEPLOY_SERVER:$WHATSUPEV_DEPLOY_UI_LOCATION
-
